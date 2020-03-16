8c687db0c526b28c78d1d9e83f1c60944d58885a
wellington-brito <were.02@hotmail.com>
2020-03-16 19:10:36 -0300

    editando layout, the hero editor, display a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-//import { MatCardModule} from '@angular/material/card';
-//import { MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeroesComponent } from './heroes/heroes.component';
 
 
@@ -18,10 +19,11 @@ import { HeroesComponent } from './heroes/heroes.component';
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
-    
-    //MatCardModule,
-    //MatInputModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 
 @Component({
@@ -7,15 +8,19 @@ import {Hero} from '../hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  
+  heroes = HEROES;
+  selectedHero: Hero;
+  hero: Hero = { id: 1, name: 'Windstorm' };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
